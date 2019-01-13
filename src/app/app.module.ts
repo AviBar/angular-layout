223b06688aa06b24ab4e612d39a8c8fb923d28ed
Avi Bar <avibargit@gmail.com>
2019-01-13 23:00:11 +0200

    initialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Layout1Component } from './layout1/layout1.component';
+import { Layout2Component } from './layout2/layout2.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Layout1Component,
+    Layout2Component,
+    FooterComponent,
+    MainComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
